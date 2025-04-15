78256bcd207cb9021e4edb72f6a4398ca3529476
Rogher Soares <rogher_adriano@hotmail.com>
2025-04-15 19:41:31 -0300

    criando a estrutura para conferir se os campos foram preenchidos

--- main.js
+++ main.js
@@ -2,4 +2,9 @@
 function acessar() {
     let loginEmail = document.getElementById("loginEmail").value;
     let loginSenha = document.getElementById("loginSenha").value;
+
+    //implementa estrutura que testa se todos os campos foram preenchidos
+    if(!loginEmail || !loginSenha) {
+        alert("Favor preencher todos os campos");
+    }
 }
